ec7f2048d447555dcc5044bd682e08bb152c0124
MyNight <59085891+MyNightDev@users.noreply.github.com>
2021-01-03 18:27:28 +0700

    Add files via upload

---
+++ ขั้นตอนวิธี/find_min_maxA.py
@@ -0,0 +1,19 @@
+
+num1 = int(input('Enter Num 1 : '))
+num2 = int(input('Enter Num 2 : '))
+num3 = int(input('Enter Num 3 : '))
+num4 = int(input('Enter Num 4 : '))
+
+_min = int(0)
+_min = num1
+
+if _min > num2:
+    _min = num2
+
+if _min > num3:
+    _min = num3
+
+if _min > num4:
+    _min = num4
+
+print(_min)
